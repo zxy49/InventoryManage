2a6172fc7259d4be6782ee1f6da90459441fea7f
zxy <491301591@qq.com>
2016-06-17 01:36:20 +0800

    dao格式调整

--- jsonDaemon/src/team/androidinventory/dao/AllocationDao.java
+++ jsonDaemon/src/team/androidinventory/dao/AllocationDao.java
@@ -5,8 +5,8 @@ import org.json.JSONObject;
 public interface AllocationDao {
 	public boolean allocationLogic(String allocationItemLists);
 	public boolean allocationJudge(String allocationItemLists);
-    public JSONObject parseAllocationJson();
-    public int getCommodityAllocationID();
+        public JSONObject parseAllocationJson();
+        public int getCommodityAllocationID();
 	public String commodityAllocation(int allocationID,int workerID);//验证allocationID之后再决定是否调拨
 	public String commodityAllocationItem(int commodityAllocationID, String commodityID, int inHouseID, int outHouseID,
 			int totalNumber,int providerID);
