5d757d6ef2eec55bc73f1553e84dd1924561cf04
zxy <491301591@qq.com>
2016-06-17 01:28:21 +0800

    统一返回类型以及方法参数

--- jsonDaemon/src/team/androidinventory/dao/OutputDao.java
+++ jsonDaemon/src/team/androidinventory/dao/OutputDao.java
@@ -3,16 +3,17 @@ package team.androidinventory.dao;
 import java.sql.Connection;
 import java.sql.Date;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import team.androidinventory.dao.impl.DBUtil;
 
 public interface OutputDao {
-	public boolean outputJudge(String sellItemLists);
+	public String outputJudge(JSONArray sellArray);
     public JSONObject parseOutputJson(String sellItemLists);
 	public int getOutputID();//验证冲突
 	public int insertIntoOutputList(int outputID,int workerID);
 	public int insertIntoSellList(int outputID, String commodityID, int houseID, int customerID, Date time,
 			int totalNumber,int providerID);
-	public String warseOut(int outputID,int workerID,String storeItemLists);
+	public String warseOut(String sellItemLists);
 }
